99af716f1eb8457705fe25a20c92c25a80a76c2f
Pilipenko_Vladislav <tokotit32@gmail.com>
2023-11-17 17:50:21 +0500

    Git и желтые окружности

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
-def main():
-    print('Hi, git')
-    print('And I change it every day')
-    print('i changed it from GitHub')
+import sys
+
+from random import randrange
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
+
+
+class MyWidget(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('second_exercise_desine.ui', self)  # Загружаем дизайн
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+        self.do_paint = False
+
+    def paint(self):
+        self.do_paint = True
+        self.update()
+
+    def draw_circle(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        k = randrange(500)
+        qp.drawEllipse(300 - k // 2, 300 - k // 2, k, k)
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = MyWidget()
+    ex.show()
+    sys.exit(app.exec_())
